72a1d05e2e7a8cfc2cf6d36dfc8fd252eb7aa4de
wsacewicz <w.sacewicz@t-bull.com>
2021-11-22 09:27:36 +0100

    changed vs code settings. Deleted empty lines in main.cpp. Changed formatting brackets.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,8 @@
 #include "include/rplidar_driver.h"
 
 #include <unistd.h>
-static inline void delay(sl_word_size_t ms)
-{
-    while (ms >= 1000)
-    {
+static inline void delay(sl_word_size_t ms) {
+    while (ms >= 1000) {
         usleep(1000 * 1000);
         ms -= 1000;
     };
@@ -22,89 +20,77 @@ using namespace sl;
 
 bool ctrl_c_pressed;
 
-int main()
-{
+int main() {
     std::cout << "Hello World" << std::endl;
 
-    const char *opt_is_channel = NULL;
-    const char *opt_channel = NULL;
-    const char *opt_channel_param_first = "/dev/ttyUSB0";
-    sl_u32 opt_channel_param_second = 0;
-    sl_u32 baudrateArray[2] = {115200, 256000};
-    int boundRate = 115200;
-    sl_result op_result;
+    const char* opt_is_channel = NULL;
+    const char* opt_channel = NULL;
+    const char* opt_channel_param_first = "/dev/ttyUSB0";
     int opt_channel_type = CHANNEL_TYPE_SERIALPORT;
+    int boundRate = 115200;
     bool connectSuccess = false;
     bool useArgcBaudrate = false;
 
-    IChannel *_channel;
-    ILidarDriver *drv = *createLidarDriver();
+    sl_u32 baudrateArray[2] = { 115200, 256000 };
+    sl_u32 opt_channel_param_second = 0;
+    sl_result op_result;
+    IChannel* _channel;
+    ILidarDriver* drv = *createLidarDriver();
     sl_lidar_response_device_info_t devinfo;
+
     _channel = (*createSerialPortChannel("/dev/ttyUSB0", 115200));
 
-    if (SL_IS_OK((drv)->connect(_channel)))
-    {
-                op_result = drv->getDeviceInfo(devinfo);
+    if (SL_IS_OK((drv)->connect(_channel))) {
+        op_result = drv->getDeviceInfo(devinfo);
 
-                if (SL_IS_OK(op_result))
-                {
-                    connectSuccess = true;
-                }
-                else
-                {
-                    delete drv;
-                    drv = NULL;
-                }
-            }
-    if (!connectSuccess)
-    {
+        if (SL_IS_OK(op_result)) {
+            connectSuccess = true;
+        }
+        else {
+            delete drv;
+            drv = NULL;
+        }
+    }
+    if (!connectSuccess) {
         std::cout << "ERROR WHILE CONNECTION";
-    }else{
-    std::cout << "CONNECTED";
+    }
+    else {
+        std::cout << "CONNECTED";
     }
 
 
-        printf("SLAMTEC LIDAR S/N: ");
-        for (int pos = 0; pos < 16; ++pos)
-        {
-            printf("%02X", devinfo.serialnum[pos]);
-        }
+    printf("SLAMTEC LIDAR S/N: ");
+    for (int pos = 0; pos < 16; ++pos) {
+        printf("%02X", devinfo.serialnum[pos]);
+    }
 
-        printf("\n"
-               "Firmware Ver: %d.%02d\n"
-               "Hardware Rev: %d\n",
-               devinfo.firmware_version >> 8, devinfo.firmware_version & 0xFF, (int)devinfo.hardware_version);
+    printf("\n"
+        "Firmware Ver: %d.%02d\n"
+        "Hardware Rev: %d\n",
+        devinfo.firmware_version >> 8, devinfo.firmware_version & 0xFF, (int)devinfo.hardware_version);
 
-
-
-        drv->setMotorSpeed();
-        drv->startScan(0, 1);
-
-
+    drv->setMotorSpeed();
+    drv->startScan(0, 1);
 }
 
-bool checkSLAMTECLIDARHealth(ILidarDriver *drv)
-{
+bool checkSLAMTECLIDARHealth(ILidarDriver* drv) {
     sl_result op_result;
     sl_lidar_response_device_health_t healthinfo;
 
     op_result = drv->getHealth(healthinfo);
-    if (SL_IS_OK(op_result))
-    { // the macro IS_OK is the preperred way to judge whether the operation is succeed.
+    if (SL_IS_OK(op_result)) { // the macro IS_OK is the preperred way to judge whether the operation is succeed.
         printf("SLAMTEC Lidar health status : %d\n", healthinfo.status);
-        if (healthinfo.status == SL_LIDAR_STATUS_ERROR)
-        {
+        if (healthinfo.status == SL_LIDAR_STATUS_ERROR) {
             fprintf(stderr, "Error, slamtec lidar internal error detected. Please reboot the device to retry.\n");
             // enable the following code if you want slamtec lidar to be reboot by software
             // drv->reset();
             return false;
         }
-        else
-        {
+        else {
             return true;
         }
     }
-    else{
+    else {
         fprintf(stderr, "Error, cannot retrieve the lidar health code: %x\n", op_result);
         return false;
     }
